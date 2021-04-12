38df73f4a7975c3c20a64fc8a7e3842f865a2abf
Kalpita <kalpitagoyal70@gmail.com>
2021-04-12 22:14:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000, function() {
+    console.log("The server has been started");
+});
